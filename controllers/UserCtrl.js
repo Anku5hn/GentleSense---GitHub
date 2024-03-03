1a1a9038c6a84211cb29c00ef118d11447c6a1b2
Anku5x <141943933+Anku5x@users.noreply.github.com>
2024-03-04 02:11:16 +0530

    Update UserCtrl.js

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -14,10 +14,10 @@ const LoginCtrl = async (req, res)=>{
     try{
         const {email, password} = req.body;
         const user = await UserModel.findOne({$and: [{email}, {password}]});
-        if(!user){
-            return res.status(404).send('User Not Found!')
-        }else{
+        if(user){
             res.status(200).json({user});
+        }else{
+             res.status(404).send('User Not Found!');
         }
     }catch(error){
         console.log(error);
